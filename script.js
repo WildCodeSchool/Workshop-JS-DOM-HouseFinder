59d1c05baa9e4157e8457e1a9828bcad7b5ae94e
KevinLavigne <lavigne.kevin02@gmail.com>
2022-09-30 11:54:57 +0200

    1st Part - step 2 : création de la fonction createCard et de la variable card, initialiser avec la balise article et son contenu (venant de index.html.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const section = document.getElementById('homes');
+
+function createCard() {
+	const card = `			
+  <article class="card-body">
+  <h1 class="card-title">this is a house</h1>
+  <h2 class="card-description">a beautiful description for a test</h2>
+  <img
+    class="card-image"
+    src="https://www.publicdomainpictures.net/pictures/90000/velka/house-illustration-clipart.jpg"
+  />
+  <button class="card-button">delete</button>
+</article>`;
+}
