bf98daca31a966df876252884a51f6d5f3861270
KevinLavigne <lavigne.kevin02@gmail.com>
2022-09-30 12:23:58 +0200

    1st Part - step 7 : Création de la variable homes contenant le jeux de donnée complète de data.js.

--- script.js
+++ script.js
@@ -7,6 +7,45 @@ const home = {
 	img: 'https://cf.bstatic.com/images/hotel/max1024x768/177/177622915.jpg',
 	available: true,
 };
+
+const homes = [
+	{
+		name: 'Modern flat',
+		type: 'House',
+		desc: "This is the perfect house for you, come to visit it you'll love it ",
+		img: 'https://cf.bstatic.com/images/hotel/max1024x768/177/177622915.jpg',
+		available: true,
+	},
+	{
+		name: 'Beautiful design house',
+		type: 'House',
+		desc: "This is the perfect house for you, come to visit it you'll love it ",
+		img: 'https://images.adsttc.com/media/images/5de3/1ca6/3312/fda8/2a00/00b3/newsletter/001.jpg?1575165037',
+		available: true,
+	},
+	{
+		name: 'Beautiful House bis',
+		type: 'House',
+		desc: "This is the perfect house for you, come to visit it you'll love it ",
+		img: 'https://assets-news.housing.com/news/wp-content/uploads/2022/01/11172338/World%E2%80%99s-15-Most-Beautiful-Houses-That-Will-Leave-You-Awestruck-featured-shutterstock_1182743467-1200x700-compressed.jpg',
+		available: false,
+	},
+	{
+		name: 'Wonderful house with Garden',
+		type: 'House',
+		desc: "This is the perfect house for you, come to visit it you'll love it ",
+		img: 'https://images.unsplash.com/photo-1585773690161-7b1cd0accfcf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+		available: true,
+	},
+	{
+		name: 'My super Flat ',
+		type: 'Flat',
+		desc: "This is the perfect flat for you, come to visit it you'll love it ",
+		img: 'https://r-cf.bstatic.com/images/hotel/max1024x768/843/84330721.jpg',
+		available: true,
+	},
+];
+
 function createCard(data) {
 	const card = `
   <article class="card-body">
