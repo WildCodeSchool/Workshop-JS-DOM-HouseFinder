d3801bcd41d050a74323e5ef4953adb376e09a04
KevinLavigne <lavigne.kevin02@gmail.com>
2022-09-30 12:02:38 +0200

    1st Part - step 3 : création de la variable div, déclaration de la valeur html de div égale card via innerHTML . Ajout de div comme enfant de section via appendChild. appel de la function createCard en bas du fichier script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const section = document.getElementById('homes');
 
 function createCard() {
-	const card = `			
+	const card = `
   <article class="card-body">
   <h1 class="card-title">this is a house</h1>
   <h2 class="card-description">a beautiful description for a test</h2>
@@ -11,4 +11,9 @@ function createCard() {
   />
   <button class="card-button">delete</button>
 </article>`;
+
+	const div = document.createElement('div');
+	div.innerHTML = card;
+	section.appendChild(div);
 }
+createCard();
